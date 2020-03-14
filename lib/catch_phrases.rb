fc6724b2b8d40c3508d87ccd04302b96d89bee51
congenial-course-2234 <jakelozano94@gmail.com>
2020-03-14 22:20:20 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,10 +8,15 @@ def toadstool
   status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
+
 def link
   status= "It's Dangerous To Go Alone! Take This."
   puts status
 end
-def all_phases(meth)
- puts meth
- end
+
+def all_phases
+  link
+  mario 
+  toadstool 
+end
+ 
